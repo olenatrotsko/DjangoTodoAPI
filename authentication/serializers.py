04a6fc120307b177ec0bfe182d7b9bc06759c4c4
trolchiha <trotsko.olena@gmail.com>
2024-04-09 21:14:48 +0300

    Added register view, url, serializer

---
+++ authentication/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from authentication.models import User
+
+
+class RegisterSerializer(serializers.Serializer):
+    
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework import response, status
 
-# Create your views here.
+from authentication.serializers import RegisterSerializer
+
+class RegisterApiView(GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            serializers.save()
+            return response.Response(serializers.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
