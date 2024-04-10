6f5295d7119f76289d98a9460e81c80addbf487f
trolchiha <trotsko.olena@gmail.com>
2024-04-10 14:51:34 +0300

    Replaced todo create and list views with listcreateapiview

--- todos/urls.py
+++ todos/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from todos import views
 
 urlpatterns = [
-    path('create', views.CreateTodoApiView.as_view(), name='create-todo'),
-    path('list', views.TodoListApiView.as_view(), name='list-todo'),
+    # path('create', views.CreateTodoApiView.as_view(), name='create-todo'),
+    # path('list', views.TodoListApiView.as_view(), name='list-todo'),
+    path('', views.TodosApiView.as_view(), name='todos'),
     
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from todos.models import Todo
 from todos.serializers import TodoSerializer
 
-class CreateTodoApiView(CreateAPIView):
-    
+class TodosApiView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    
-class TodoListApiView(ListAPIView):
-    serializer_class = TodoSerializer
-    permission_classes = (IsAuthenticated,)
-    queryset = Todo.objects.all()
 
-    def get_queryset(self):
-        return Todo.objects.filter(owner=self.request.user)
+# class CreateTodoApiView(CreateAPIView):
+    
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated,)
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+    
+# class TodoListApiView(ListAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated,)
+#     queryset = Todo.objects.all()
+
+#     def get_queryset(self):
+#         return Todo.objects.filter(owner=self.request.user)
     
