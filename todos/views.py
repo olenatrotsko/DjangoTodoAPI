952eea7fcfe358e6f42712028aefcaf8ce71c5a3
trolchiha <trotsko.olena@gmail.com>
2024-04-10 19:03:35 +0300

    Added filtering and ordering for todos

--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,17 @@
+from rest_framework import filters
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from todos.models import Todo
 from todos.serializers import TodoSerializer
+
 
 class TodosApiView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['id', 'title', 'is_completed']
+    search_fields = ['id', 'title', 'description', 'is_completed']
 
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
