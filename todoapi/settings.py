3e1c0f0facfc636c7b99579da531e646bc8e260f
trolchiha <trotsko.olena@gmail.com>
2024-04-11 10:47:52 +0300

    Added swagger

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'django_filters',
     'authentication',
     'todos',
